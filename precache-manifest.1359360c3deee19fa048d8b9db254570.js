728172fdc7648708b0c27292bbf53149f05ba0ab
talJoffeExelate <tal.joffe@nielsen.com>
2019-08-12 09:32:40 +0300

    Updates

---
+++ precache-manifest.1359360c3deee19fa048d8b9db254570.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698b1ea612725d4107a3bdc40a8a8b28",
+    "url": "/permutations/index.html"
+  },
+  {
+    "revision": "6b4375b3f5c7233335d3",
+    "url": "/permutations/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "fb3d5f1f0f82d416586e",
+    "url": "/permutations/static/js/2.b6b225f4.chunk.js"
+  },
+  {
+    "revision": "6b4375b3f5c7233335d3",
+    "url": "/permutations/static/js/main.bae0d182.chunk.js"
+  },
+  {
+    "revision": "b2ccb5fd847f0c0bf37a",
+    "url": "/permutations/static/js/runtime~main.fb5c2bfc.js"
+  }
+]);
